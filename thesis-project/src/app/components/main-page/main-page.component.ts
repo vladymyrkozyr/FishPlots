a8ebfc9b1ccf51ef31ea103c95651d85b85de9fe
vladymyrkozyr <vladymyrkozyr@gmail.com>
2020-11-02 20:59:43 -0800

    load json data into client

--- thesis-project/src/app/components/main-page/main-page.component.ts
+++ thesis-project/src/app/components/main-page/main-page.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-import { DataService } from 'src/app/services/data.service';
-import { forkJoin } from 'rxjs';
+import { DataService } from "src/app/services/data.service";
+import { forkJoin } from "rxjs";
 am4core.useTheme(am4themes_animated);
 
 @Component({
-	selector: 'main-page',
-	templateUrl: './main-page.component.html',
-	styleUrls: ['./main-page.component.scss']
+	selector: "main-page",
+	templateUrl: "./main-page.component.html",
+	styleUrls: ["./main-page.component.scss"]
 })
 export class MainPageComponent implements OnInit {
 
@@ -88,6 +88,7 @@ export class MainPageComponent implements OnInit {
 	}
 
 	dataQuantities = {};
+	dataValues = {};
 	summarizedDataByYearsAndProvinces = {};
 
 	constructor(
@@ -97,33 +98,16 @@ export class MainPageComponent implements OnInit {
 	ngOnInit(): void { }
 
 	getTestFile(): void {
-		// this.dataService.getTestFile().subscribe(r => {
-		// 	let chart: am4charts.PieChart = am4core.create("chartdiv", am4charts.PieChart);
-		// 	var pieSeries = chart.series.push(new am4charts.PieSeries());
-		// 	let data = r.map(d => <any>{ fishType: d["Species"], value: String(d["British Columbia"]) });
-		// 	console.log(data.map(d => <any>{ ...d, value: parseFloat(d.value != "-" ? d.value.replace(',', '') : 0) }))
-		// 	data = data.map(d => <any>{ ...d, value: parseFloat(d.value != "-" ? d.value.replace(',', '') : 0) });
-		// 	// r.map(d=><any>{fishType:d["Species"], value: d["British Columbia"].replace(",", "")})
-		// 	chart.data = data;
-		// 	pieSeries.dataFields.value = "value";
-		// 	pieSeries.dataFields.category = "fishType";
-		// 	pieSeries.slices.template.stroke = am4core.color("#fff");
-		// 	pieSeries.slices.template.strokeWidth = 2;
-		// 	pieSeries.slices.template.strokeOpacity = 1;
 
-		// 	// This creates initial animation
-		// 	pieSeries.hiddenState.properties.opacity = 1;
-		// 	pieSeries.hiddenState.properties.endAngle = -90;
-		// 	pieSeries.hiddenState.properties.startAngle = -90;
-		// });
-
-		let fileRequests = [];
+		let fileRequestsQuantities = [];
+		let fileRequestsValues = [];
 
 		for (let i: number = 1990; i <= 2018; i++) {
-			fileRequests.push(this.dataService.getFileByUrl(`assets/files/json/sea fish quantities/s${i}pq_e.json`));
+			fileRequestsQuantities.push(this.dataService.getFileByUrl(`assets/files/json/sea fish quantities/s${i}pq_e.json`));
+			fileRequestsValues.push(this.dataService.getFileByUrl(`assets/files/json/sea fish values/s${i}pv_e.json`));
 		}
 
-		forkJoin(fileRequests).subscribe((files: any[]) => {
+		forkJoin([...fileRequestsQuantities, ...fileRequestsValues]).subscribe((files:any[]) => {
 
 			console.log(files)
 
@@ -137,9 +121,20 @@ export class MainPageComponent implements OnInit {
 					"Newfoundland and Labrador": this.parseStringValue(d["Newfoundland and Labrador"]),
 					"British Columbia": this.parseStringValue(d["Nova Scotia"])
 				}).filter(d => d.fishType != "");
+
+				this.dataValues[i + 1990] = files[i + 29].map(d => <any>{
+					fishType: d["Species"],
+					"Nova Scotia": this.parseStringValue(d["Nova Scotia"]),
+					"New Brunswick": this.parseStringValue(d["New Brunswick"]),
+					"Prince Edward Island": this.parseStringValue(d["Prince Edward Island"]),
+					"Quebec": this.parseStringValue(d["Quebec"]),
+					"Newfoundland and Labrador": this.parseStringValue(d["Newfoundland and Labrador"]),
+					"British Columbia": this.parseStringValue(d["Nova Scotia"])
+				}).filter(d => d.fishType != "");
 			}
 
 			console.log(this.dataQuantities);
+			console.log(this.dataValues);
 
 			this.lineChartSummarizedByProvinces();
 		});
